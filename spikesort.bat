6338c2056d97536780436adcaa32f30da51034c9
padillacoreanolab <padillacoreanolab@gmail.com>
2023-06-29 16:28:49 -0400

    docker pipeline working updating gradio app next

--- spikesort.bat
+++ spikesort.bat
@@ -1,7 +1,28 @@
 @echo off
 
-echo Pulling latest version of ...
-docker pull spikesort:latest
+echo Logging into Docker...
+docker login
+if %errorlevel% neq 0 (
+    echo Login failed!
+    exit /b %errorlevel%
+)
 
-echo Running Docker Compose...
-docker-compose -f docker-compose.yml up -d
+@REM docker login  --username padillacoreanolab --password dckr_pat_colSv-sFqYi6S29DFTRN5Mycjz4
+
+echo Pulling latest version of ...
+docker pull padillacoreanolab/spikesort:latest
+
+echo Shutting Down Open Docker Container ...
+docker stop spikesort_c
+docker rm spikesort_c
+
+echo Starting Docker Container ...
+docker-compose -f docker-compose.yml up -d && (
+    start http://localhost:7000
+)
+
+pause
+
+echo Shutting Down Open Docker Container ...
+docker stop spikesort_c
+docker rm spikesort_c

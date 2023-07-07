49aff6b7c6f154d18750f4c0dff89eda7474af98
padillacoreanolab <padillacoreanolab@gmail.com>
2023-07-07 15:20:05 -0400

    Update spikesort.bat

--- spikesort.bat
+++ spikesort.bat
@@ -8,13 +8,14 @@ if %errorlevel% neq 0 (
 )
 
 echo Pulling Latest Version of Image: spikesort...
-docker pull padillacoreanolab/spikesort:latest
+docker pull padillacoreanolab/spikesort:latest || goto catch
+:catch
 
 echo Shutting Down Open Docker Containers With The Same Name ...
 docker stop spikesort_c
 docker rm spikesort_c
 
-set /p path_variable="ENTER THE FULL PATH TO THE PARENT FOLDER OF THE SPIKESORT DATA FOLDER: "
+set /p path_variable="FULL PATH TO THE PARENT FOLDER OF THE SPIKESORT DATA FOLDER: "
 
 echo Running Docker Container:  spikesort_c...
 @REM docker-compose -f docker-compose.yml up -d
